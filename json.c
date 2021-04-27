c15d87b91aeef597b15b1c353fda4f90fc2d4bc1
Martin Pandola <martinpandola@gmail.com>
2021-04-27 10:16:50 -0500

    json

--- json.c
+++ json.c
@@ -1,123 +1,174 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "duktape.h"
+#include "json.h"
 
-void martin_parse_json(duk_context *ctx, char *str, int len) {
-    int i, strPtr, writePtr;
-    bool inString;
-    int stackDepth;
-
-    stackDepth = 0;
-	for(i=0; i<len; i++){
-	    printf("\nstr %d/%d %c %s\n", i, len, str[i], &str[i]);
-	    printf("stackDepth %d\n", stackDepth);
-        switch(str[i]){
+void martin_parse_json_eat_whitespace(char *str, int *i) {
+    for(;;){
+        switch(str[*i]){
         case 0x09:
         case 0x0A:
         case 0x0D:
         case 0x20:
-            // whitespace
+            (*i)++;
             break;
-        case '[':
-            // array begin
-            break;
-        case ']':
-            // array finish - stack may have pending value on it
-            break;
-        case '{':
-            // object begin
-            duk_push_object(ctx);
-            stackDepth++;
-            break;
-        case ':':
-            // object - stack has key on it
-            break;
-        case '}':
-            // object finish - stack may have pending key and value on it
-            if(stackDepth > 1){
-                duk_put_prop(ctx, -3);
-                stackDepth -= 2;
-            }
-            break;
-        case ',':
-            // object - stack has key and value on it
-            // or array - stack has value on it
-            duk_put_prop(ctx, -3);
-            break;
+        default:
+            return;
+        }
+    }
+}
+
+void martin_parse_json_string(duk_context *ctx, char *str, int *i) {
+    int writePtr;
+
+    writePtr = 0;
+    (*i)++; // initial double-quote
+    for(;;){
+        switch(str[*i]){
         case '"':
-            // string
-            inString = true;
-            writePtr = 0;
-            for(strPtr=1; inString; strPtr++){
-                switch(str[i+strPtr]){
-                case '"':
-                    duk_push_lstring(ctx, str, writePtr);
-                    stackDepth++;
-                    inString = false;
-                    break;
-                case '\\':
-                    // escaped char
-                    strPtr++;
-                    switch(str[i+strPtr]){
-                    case '"':
-                        str[writePtr++] = '"';
-                        break;
-                    case '\\':
-                        str[writePtr++] = '\\';
-                        break;
-                    case '/':
-                        str[writePtr++] = '/';
-                        break;
-                    case 'b':
-                        str[writePtr++] = '\b';
-                        break;
-                    case 'f':
-                        str[writePtr++] = '\f';
-                        break;
-                    case 'n':
-                        str[writePtr++] = '\n';
-                        break;
-                    case 'r':
-                        str[writePtr++] = '\r';
-                        break;
-                    // default case not checked
-                    } // end escaped char switch
-                    break;
-                default:
-                    str[writePtr++]=str[i+strPtr];
-                    break;
-                }
-            } // end inString for
-            i += strPtr-1;
-            break; // end parse string
-        case '-':
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-            // number
+            duk_push_lstring(ctx, str, writePtr);
+            (*i)++; // final double-quote
+            return;
+        case '\\':
+            // escaped char
+            (*i)++;
+            switch(str[*i]){
+            case '"':
+                str[writePtr++] = '"';
+                break;
+            case '\\':
+                str[writePtr++] = '\\';
+                break;
+            case '/':
+                str[writePtr++] = '/';
+                break;
+            case 'b':
+                str[writePtr++] = '\b';
+                break;
+            case 'f':
+                str[writePtr++] = '\f';
+                break;
+            case 'n':
+                str[writePtr++] = '\n';
+                break;
+            case 'r':
+                str[writePtr++] = '\r';
+                break;
+            // TODO unicode case
+            // default case not checked
+            }
+            (*i)++;
             break;
-        case 't':
-            // true
-            duk_push_true(ctx);
-            i += 4;
-            break;
-        case 'f':
-            // false
-            duk_push_false(ctx);
-            i += 5;
-            break;
-        case 'n':
-            // null
-            duk_push_null(ctx);
-            i += 4;
+        default:
+            str[writePtr++]=str[*i];
+            (*i)++;
             break;
         }
     }
+}
+
+void martin_parse_json_number(duk_context *ctx, char *str, int *i) {
+    char *end;
+    double num;
+
+    num = strtod(&str[*i], &end);
+    duk_push_number(ctx, num);
+    while(&str[*i]!=end)(*i)++;
+}
+
+void martin_parse_json_array(duk_context *ctx, char *str, int *i) {
+    int arrIdx, index;
+
+    index = 0;
+    arrIdx = duk_push_array(ctx);
+    (*i)++; // open bracket
+    for(;;){
+        martin_parse_json_eat_whitespace(str, i);
+        switch(str[*i]){
+        case ']':
+            (*i)++; // close bracket
+            return;
+        case ',':
+            (*i)++;
+            break;
+        default:
+            martin_parse_json_value(ctx, str, i);
+            duk_put_prop_index(ctx, arrIdx, index);
+            index++;
+            break;
+        }
+    }
+}
+
+void martin_parse_json_object(duk_context *ctx, char *str, int *i) {
+    int objIdx;
+
+    objIdx = duk_push_object(ctx);
+    (*i)++; // open curly
+    for(;;){
+        martin_parse_json_eat_whitespace(str, i);
+        switch(str[*i]){
+        case '}':
+            (*i)++; // close curly
+            return;
+        case ',':
+            (*i)++;
+            break;
+        case '"':
+            martin_parse_json_string(ctx, str, i);
+            (*i)++; // colon
+            martin_parse_json_value(ctx, str, i);
+            duk_put_prop(ctx, objIdx);
+            break;
+        }
+    }
+}
+
+void martin_parse_json_value(duk_context *ctx, char *str, int *i) {
+    martin_parse_json_eat_whitespace(str, i);
+    switch(str[*i]){
+    case '-':
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+        martin_parse_json_number(ctx, str, i);
+        break;
+    case '"':
+        martin_parse_json_string(ctx, str, i);
+        break;
+    case '{':
+        martin_parse_json_object(ctx, str, i);
+        break;
+    case '[':
+        martin_parse_json_array(ctx, str, i);
+        break;
+    case 't':
+        // true
+        duk_push_true(ctx);
+        (*i) += 4;
+        break;
+    case 'f':
+        // false
+        duk_push_false(ctx);
+        (*i) += 5;
+        break;
+    case 'n':
+        // null
+        duk_push_null(ctx);
+        (*i) += 4;
+        break;
+    }
+    martin_parse_json_eat_whitespace(str, i);
+}
+
+void martin_parse_json(duk_context *ctx, char *str, int len) {
+    int i = 0;
+    martin_parse_json_value(ctx, str, &i);
 }
--- json.h
+++ json.h
@@ -3,6 +3,10 @@
 
 #include "duktape.h"
 
+void martin_parse_json_eat_whitespace(char *str, int *i);
+void martin_parse_json_string(duk_context *ctx, char *str, int *i);
+void martin_parse_json_object(duk_context *ctx, char *str, int *i);
+void martin_parse_json_value(duk_context *ctx, char *str, int *i);
 void martin_parse_json(duk_context *ctx, char *str, int len);
 
 #endif
